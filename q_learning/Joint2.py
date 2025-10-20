87d178c46a043207fc2bd8d65f8b64c326e9e3a7
颜廷震 <yantingzhen@shins-MacBook-Air.local>
2025-10-20 00:08:54 -0400

    para: episodes 120000

--- q_learning/Joint2.py
+++ q_learning/Joint2.py
@@ -748,7 +748,7 @@ def save_qtables(env, agent1, agent2,
 if __name__ == "__main__":
     env, agent1, agent2, rewards, steps, mean_q1, mean_q2, epsilons = \
         train_two_agents_representative(
-            episodes_per_start=80000,  # 8–20 works well on 4x4
+            episodes_per_start=120000,  # 8–20 works well on 4x4
             map_size=4,
             seed=123,
             max_steps=30,
