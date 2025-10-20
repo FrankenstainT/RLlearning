09ece199f428fd4e4378c3a385fdf2a87a09f759
颜廷震 <yantingzhen@shins-MacBook-Air.local>
2025-10-20 01:04:11 -0400

    para: episodes 300000, step penalty -.15

--- q_learning/Joint2.py
+++ q_learning/Joint2.py
@@ -37,7 +37,7 @@ class TwoAgentFrozenLake:
         self.n_actions = 4
         self.goal = (self.n - 1, self.n - 1)
         self.current_state = None
-        self.step_penalty = -0.12
+        self.step_penalty = -0.15
         self.stay_penalty = -.5
 
     # ----- Coordinate helpers -----
@@ -748,7 +748,7 @@ def save_qtables(env, agent1, agent2,
 if __name__ == "__main__":
     env, agent1, agent2, rewards, steps, mean_q1, mean_q2, epsilons = \
         train_two_agents_representative(
-            episodes_per_start=120000,  # 8–20 works well on 4x4
+            episodes_per_start=300000,  # 8–20 works well on 4x4
             map_size=4,
             seed=123,
             max_steps=30,
